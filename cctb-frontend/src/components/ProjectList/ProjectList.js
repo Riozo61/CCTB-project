6ae4c7814191fda0726e066937a7bcf2528c7883
Riozo61 <abond6600@gmail.com>
2021-11-05 19:01:36 +0300

    Страницы проекта

--- cctb-frontend/src/components/ProjectList/ProjectList.js
+++ cctb-frontend/src/components/ProjectList/ProjectList.js
@@ -1,35 +1,19 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import { Context } from '../../index';
-import { Divider, ListItem } from "@mui/material";
-import { Checkbox } from "@mui/material";
-
-
-
+import { useHistory } from 'react-router';
+import ProjectItem from './ProjectItem';
 
 
 const ProjectList = observer( () => {
   const { project } = useContext(Context);
-
+  const history = useHistory();
+  console.log(history)
 
   return (
     <div>
-      {project.project.map((p) => (
-        <div key={p.id}>
-        <ListItem disablePadding>
-          <ListItemButton>
-          <Checkbox checked={p.checked} />
-            <ListItemText primary={p.nameProject} secondary='Название проекта' />
-            <ListItemText primary={p.customer} secondary='Заказчик'/>
-            <ListItemText primary={p.address} secondary='Объект'/>
-            <ListItemText primary={p.timeline} secondary='Сроки' />
-            <ListItemText primary={p.estimation} secondary='Расчет'/>
-          </ListItemButton>
-        </ListItem>
-        <Divider/>
-        </div>
+      {project.project.map((proj) => (
+          <ProjectItem key={proj.id} project={proj}/>
       ))}
     </div>
   )
