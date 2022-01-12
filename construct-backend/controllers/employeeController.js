5dcda8eaca1224dae09ca8e98285076620f1b8b4
Ordus161 <makc1992@list.ru>
2022-01-12 16:57:22 +0300

    Правки

--- construct-backend/controllers/employeeController.js
+++ construct-backend/controllers/employeeController.js
@@ -5,29 +5,29 @@ const ApiError = require('../error/ApiError');
 
 class EmployeeController {
     async create(req, res, next) {
-        const { email, firstName, lastName, role, phone, salary, type } = req.body
-        if (!email || !firstName || !lastName || !role || !phone || !salary || !type) {
-            return next(ApiError.badRequest('Заполните обязательные поля'))
-        }
-        const empl = await Employee.Employee.findOne({ where: { email } })
-        if (empl) {
-            return next(ApiError.badRequest('Сотрудник уже зарегистрирован'))
-        }
+            const {email,firstName,lastName,role,phone,salary,type,currency} = req.body
+            if (!email||!firstName||!lastName||!role||!phone||!salary||!type||!currency ) {
+                return next(ApiError.badRequest('Заполните обязательные поля'))
+            }
+            const empl = await Employee.Employee.findOne({where: {email}})
+            if (empl) {
+                return next(ApiError.badRequest('Сотрудник уже зарегистрирован'))
+            }
 
-        const employee = await Employee.Employee.create({ email, firstName, lastName, role, phone, salary, type })
-
-        return res.json(employee)
-    }
+            const employee = await Employee.Employee.create({email,firstName,lastName,role,phone,salary,type,currency})
+        
+            return res.json(employee)
+        }
 
     async getAll(req, res) {
-        let { limit, page } = req.query
+        let {limit, page} = req.query
         page = page || 1
-        limit = limit || 9
+        limit = limit || 100
         let offset = page * limit - limit
         let Employees;
-        Employees = await Employee.Employee.findAndCountAll({ limit, offset })
+        Employees = await Employee.Employee.findAndCountAll({limit, offset})
         return res.json(Employees)
-
+       
     }
 
 
